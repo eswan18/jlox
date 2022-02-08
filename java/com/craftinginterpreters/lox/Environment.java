549276b227bc35430aac23073d5eb6d84dfa6fc6
Ethan Swan <eswan18@rocketmail.com>
2022-02-07 20:02:39 -0600

    Update environments to support nesting.

--- java/com/craftinginterpreters/lox/Environment.java
+++ java/com/craftinginterpreters/lox/Environment.java
@@ -4,7 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Environment {
+    final Environment enclosing;
     private final Map<String, Object> values = new HashMap<>();
+
+    Environment() {
+        enclosing = null;
+    }
+
+    Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
 
     void define(String name, Object value) {
         values.put(name, value);
@@ -14,6 +23,10 @@ class Environment {
         if (values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
         }
+
+        // Look at parent environment(s) if the variable isn't in this one.
+        if (enclosing != null) return enclosing.get(name);
+
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
 
@@ -23,6 +36,12 @@ class Environment {
             return;
         }
 
+        // Look at parent environment(s) if the variable isn't in this one.
+        if (enclosing != null) {
+            enclosing.assign(name, value);
+            return;
+        }
+        
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
 
